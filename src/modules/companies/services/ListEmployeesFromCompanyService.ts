d0bf345a52f996fad82cafbbacce7afa50392ca9
RiadOliveira <riad.oliveira@hotmail.com>
2022-11-06 03:01:16 -0300

    List employees ordered by producer

--- src/modules/companies/services/ListEmployeesFromCompanyService.ts
+++ src/modules/companies/services/ListEmployeesFromCompanyService.ts
@@ -1,4 +1,5 @@
 import User from 'typeorm/entities/User';
+import { AccountType } from 'types/AccountType';
 import CompanyService from './CompanyService';
 
 export default class ListEmployeesFromCompanyService extends CompanyService {
@@ -7,6 +8,15 @@ export default class ListEmployeesFromCompanyService extends CompanyService {
             await this.companiesRepository.findAllEmployeesFromCompany(
                 companyId,
             );
+        const producerIndex = findedEmployees.findIndex(
+            ({ accountType }) => accountType === AccountType.PRODUCER,
+        )!;
+
+        if (producerIndex !== 0) {
+            const firstEmployee = findedEmployees[0];
+            findedEmployees[0] = findedEmployees[producerIndex];
+            findedEmployees[producerIndex] = firstEmployee;
+        }
 
         return findedEmployees;
     }
