062970717767176a625223889f60a43d0046ad76
RiadOliveira <riad.oliveira@hotmail.com>
2022-11-07 10:59:40 -0300

    Date Transforms month fixed

--- src/utils/formatDateWithoutTimezone.ts
+++ src/utils/formatDateWithoutTimezone.ts
@@ -1,7 +1,7 @@
 export const formatDateWithoutTimezone = (date: Date) => {
     const parsedDate = new Date(date);
-    const day = parsedDate.getUTCDate().toString().padStart(2, '0');
-    const month = parsedDate.getUTCMonth().toString().padStart(2, '0');
+    const day = String(parsedDate.getUTCDate()).padStart(2, '0');
+    const month = String(parsedDate.getUTCMonth() + 1).padStart(2, '0');
     const year = parsedDate.getUTCFullYear();
 
     return `${day}/${month}/${year}`;
